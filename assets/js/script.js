976160da82dc713edc467cc63cef535f4c055515
divansh-529 <divanshsachdev@gmail.com>
2024-08-02 11:41:22 +0530

    added / changed section two slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,3 +33,49 @@ $('.cards-parent-section-one').slick({
    
   ]
 });
+
+
+
+$(".slick-slider-parent").slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        dots: true,
+      },
+    },
+  ],
+});
